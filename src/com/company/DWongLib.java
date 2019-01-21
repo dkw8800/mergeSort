30808b64e3068e9ea4e2f0a7bff445001da4c111
dwong0023 <dwong0023@bths.edu>
2019-01-21 12:04:11 -0500

    c

--- src/com/company/DWongLib.java
+++ src/com/company/DWongLib.java
@@ -21,7 +21,42 @@ public class DWongLib {
 
     public void merge(int[] arr, int left, int mid, int right, int[] temp)
     {
+        {
+            int i = left;
+            int j = mid + 1;
+            int k = left;
+            while (i <= mid && j <= right)
+            {
+                if (arr[i] < arr[j])
+                {
+                    temp[k] = arr[i];
+                    i++;
+                }
+                else
+                {
+                    temp[k] = arr[j];
+                    j++;
+                }
+                k++;
+            }
 
+            while (i <= mid)
+            {
+                temp[k] = arr[i];
+                i++;
+                k++;
+            }
+            while (j <= right)
+            {
+                temp[k] = arr[j];
+                j++;
+                k++;
+            }
+            for (k = left; k <= right; k++)
+            {
+                arr[k] = temp[k];
+            }
+        }
     }
 
 }
